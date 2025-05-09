5ec436b7fa3299b7762a286762b2d4e19f493d0e
khaenzelidorothy <khaenzelidorothy@gmail.com>
2025-05-09 17:29:52 +0300

    working on classes

--- index.js
+++ index.js
@@ -15,41 +15,46 @@ FeatureToggle.prototype.canAccess = function(userRole){
     }
 }
 FeatureToggle.prototype.toggleFeature = function(flag){
-    if(flag == "true"){
-        
-    }
-}
-FeatureToggle.prototype.accessAttempts = function(){
+    return this.isEnabled = flag;
     
 }
+const featuretoggle = new FeatureToggle("signup",true,["admin","betaTesters"])
+console.log(featuretoggle.canAccess("developer"));
+console.log(featuretoggle.canAccess("betaTesters"));
+console.log(featuretoggle.toggleFeature(true));
 
-const featuretoggle = new FeatureToggle("signup","true",[""])
 
 //In a freelancer time-tracking platform, create a TimeLog constructor function with properties: freelancerName (string), projectDetails
 //  (object with name and hourlyRate), and logs (array of objects with date, hoursWorked), then add prototype methods to calculate total
 //  earnings, filter logs by date range, and determine if weekly hours exceed 40 using if-else logic.
 
-// function Timelog(freelancerName,projectDetails,logs){
-//     this.freelancerName = freelancerName;
-//     this.projectDetails = projectDetails;
-//     this.logs = logs;
-// }
+function Timelog(freelancerName,projectDetails,logs){
+    this.freelancerName = freelancerName;
+    this.projectDetails = projectDetails;
+    this.logs = logs;
+}
 // Timelog.prototype.totalEarnings = function(){
-//     let earnings = this.logs.hoursWorked.reduce((acc,curr)=>acc+curr,0)
-//     return earnings*this.projectDetails.hourlyRate;
+//     let earnings = this.logs.hoursWorked
+//     let sum = earnings.reduce((acc,curr)=>acc+curr,0);
+//     return sum*this.projectDetails.hourlyRate;
 // }
-// Timelog.prototype.filterLog = function(){
-// //    let filteredDate= this.logs.date.filter(14<date<20)
-//    if(earnings>40){
-//     return "exceed";
-//    }
-//    else{
-//     return "not exceeded"
-//    }
-// }
-// const timelog = new Timelog("John",{name:"peter",hourlyRate:35.0},[{date:14,hoursWorked:16},{date:17,hoursWorked:22}])
-// console.log(timelog.totalEarnings());
-// console.log(timelog.filterLog());
+
+Timelog.prototype.totalEarnings = function (){
+ let sum = 0;
+ for (log of this.logs){
+    return sum += this.logs.hoursWorked * this.projectDetails.hourlyRate
+ }}
+Timelog.prototype.filterLog = function(){
+   if(this.summation > 40){
+    return "exceed";
+   }
+   else{
+    return "not exceeded"
+   }
+}
+const timelog = new Timelog("John",{name:"peter",hourlyRate:35.0},[{date:14,hoursWorked:16},{date:17,hoursWorked:22}])
+console.log(timelog.totalEarnings());
+console.log(timelog.filterLog());
 
 
 
@@ -80,7 +85,7 @@ Order.prototype.orderStatus = function(payment){
     }
 }
 Order.prototype.orderUrgency = function(){
-    if(quantity >= 25){
+    if(this.quantity >= 25){
         return "High priority"
     }
     else{
@@ -99,23 +104,47 @@ console.log(order.orderUrgency());
 // with keys like communication, efficiency, and reliability), and feedback (array of strings), then use prototypes to calculate an average
 //  score, classify performance level using control flow, and add new feedback based on conditions.
 
-function Employee(id,name,performanceMetrics,feedback){
+class Employee{
+constructor(id,name,performanceMetrics,feedback){
     this.id = id;
     this.name = name;
     this.performanceMetrics = performanceMetrics;
     this.feedback = feedback;
-}
+}}
 Employee.prototype.averageScore = function(){
-
+const average = Object.values(this.performanceMetrics)
+sum = average.reduce((acc,curr) => acc + curr,0)
+return sum/average.length
 }
 Employee.prototype.performanceLevel = function(){
-
+if(this.averageScore() == 70){
+    return "Exceeded expectation"
 }
+else if(this.averageScore>=50 && this.averageScore< 70){
+    return "average score"
+}
+else{
+    return "low score"
+}}
 Employee.prototype.addFeature = function(){
-
+    if(this.performanceLevel() == "Exceeded expectation"){
+        return "You've passed"
+    }
+    else if(this.performanceLevel() == "average score"){
+        return "Good trial"
+    }
+    else{
+        return "Do better"
+    }
 }
 
-const employee = new Employee(203,"Lennox",{communication:"good",efficiency:"fluent",reliability:"very reliable"},["need","bad","easy"])
+const employee = new Employee(203,"Lennox",{communication:75,efficiency:65,reliability:82},["need","bad","easy"])
+console.log(employee.averageScore());
+console.log(employee.performanceLevel());
+console.log(employee.addFeature());
+
+
+
 
 
 //Build a simple e-learning system where a Course class has properties: title (string), instructor (object with name and expertise), and 
@@ -129,25 +158,25 @@ class Course{
     this.students = students;
 }}
 Course.prototype.studentsName = function(){
-    if(this.students.completionStatus=="completed"){
-        return this.students.name
-    }
+    let studentsCompleted =this.students.filter(student =>student.completionStatus === "completed");
+    return studentsCompleted;
 }
-Course.prototype.countEnrolled = function(){
-     let count = 0;
-    let counting = this.students.forEach(student =>{
-        student.name =
-         count ++
-    })
-    return counting
+Course.prototype.countEnrolled = function(expertiseArea){
+    return this.students.filter(student =>this.instructor.expertise === expertiseArea).length;
 }
 Course.prototype.instructorMessage = function(){
-
+    let numberStudent = this.students.length
+    if(numberStudent>=5){
+        return `${this.instructor.name} you have ${numberStudent} students.`
+    }
+    else{
+        return `${this.instructor.name} you have ${numberStudent} students.` 
+    }
 }
 
-const course = new course("Biology",{name:"Nelly",expertise:"plantology"},[{name:"kevin",completionStatus:"completed"},{name:"merry",completionStatus:"not completed"}]);
+const course = new Course("Biology",{name:"Nelly",expertise:"plantology"},[{name:"kevin",completionStatus:"completed"},{name:"merry",completionStatus:"not completed"},{name:"joseph",completionStatus:"completed"}]);
 console.log(course.studentsName());
-console.log(course.countEnrolled());
+console.log(course.countEnrolled("plantology"));
 console.log(course.instructorMessage());
 
 
